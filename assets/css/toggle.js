a82a5efa9e50ae99904aeb43b5f1b8c3944c5460
amosjerbi <amosjerbi@gmail.com>
2020-01-21 21:08:04 +0200

    Add files via upload
    Accordion1

--- assets/css/toggle.js
+++ assets/css/toggle.js
@@ -17,3 +17,25 @@ var checkbox = document.querySelector('input[name=theme]');
                 document.documentElement.classList.remove('transition')
             }, 1000)
         }
+        
+        //Accordion //
+        
+(function($) {
+  
+    $('.accordion a').click(function(j) {
+        var dropDown = $(this).closest('li').find('p');
+
+        $(this).closest('.accordion').find('p').not(dropDown).slideUp();
+
+        if ($(this).hasClass('active')) {
+            $(this).removeClass('active');
+        } else {
+            $(this).closest('.accordion').find('a.active').removeClass('active');
+            $(this).addClass('active');
+        }
+
+        dropDown.stop(false, true).slideToggle();
+
+        j.preventDefault();
+    });
+})(jQuery);
