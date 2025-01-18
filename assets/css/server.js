e8b8557c7bdcb2c76bd3dc83d34cddfeadac0778
amosjerbi <amosjerbi@gmail.com>
2025-01-18 15:41:54 +0200

    Add files via upload

--- assets/css/server.js
+++ assets/css/server.js
@@ -3,6 +3,6 @@ const express = require('express');
 const app = express();
 
 app.get('/api/instagram', async (req, res) => {
-    const token = process.env.instagramToken;
+    const token = process.env.INSTAGRAM_ACCESS_TOKEN;
     // Make Instagram API calls here
 }); 
