3d8b215753bb3ef6deb7b9cc9de3321a76054be9
amosjerbi <amosjerbi@gmail.com>
2025-01-19 19:04:44 +0200

    Update server.js

--- assets/css/server.js
+++ assets/css/server.js
@@ -3,6 +3,6 @@ const express = require('express');
 const app = express();
 
 app.get('/api/instagram', async (req, res) => {
-    const token = ${{ secrets.INSTAGRAM_ACCESS_TOKEN }};
+    const instagramToken = process.env.INSTAGRAM_ACCESS_TOKEN;
     // Make Instagram API calls here
 }); 
