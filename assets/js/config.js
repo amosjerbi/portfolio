2044f58bc119f1a9b634f90f2fd3952975498433
amosjerbi <amosjerbi@gmail.com>
2025-01-22 00:11:55 +0200

    Refactor: Move JavaScript files to assets/js directory and implement ES modules

---
+++ assets/js/config.js
@@ -0,0 +1,9 @@
+// Instagram Gallery Configuration
+const config = {
+    postsPerPage: 30,
+    maxPosts: 120,
+    apiFields: 'id,caption,media_type,media_url,permalink,timestamp',
+    allowedMediaTypes: ['IMAGE', 'CAROUSEL_ALBUM']
+};
+
+export default config;
--- config.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config();
-
-const fs = require('fs');
-
-const configContent = `window.INSTAGRAM_ACCESS_TOKEN = '${process.env.INSTAGRAM_ACCESS_TOKEN}';`;
-
-fs.writeFileSync('../config.js', configContent);
-console.log('Config file generated successfully!');
